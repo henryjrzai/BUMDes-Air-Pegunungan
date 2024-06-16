a7d24009defc976c7e584132616a9da04f27da3d
Henry Junior Zai <hjuniorzai@gmail.com>
2024-06-16 22:59:14 +0700

    [FIX] the migration of the customer table so that it is related to water_tarifs"

--- database/migrations/2024_06_05_201204_create_customers_table.php
+++ database/migrations/2024_06_05_201204_create_customers_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('dusun');
             $table->string('rt');
             $table->string('rw');
-            $table->foreignId('water_tarif_id')->constrained(table : 'water_tarifs')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_06_16_155454_add_water_tarif_id_to_customers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->foreignId('water_tarif_id')->after('meter_id')->constrained(table : 'water_tarifs')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            //
+        });
+    }
+};
