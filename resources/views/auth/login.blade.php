b065b4481bfe895a2d919be25d01c363ff5461ec
Henry Junior Zai <hjuniorzai@gmail.com>
2024-06-16 23:43:20 +0700

    [FIX] update logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
                         <div class="card mb-0">
                             <div class="card-body">
                                 <a class="text-nowrap logo-img text-center d-block py-3 w-100">
-                                    <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
+                                    <img src="../assets/images/logos/BUMDes Air Pegunungan.png" width="180" alt="">
                                 </a>
                                 <p class="text-center">Your Social Campaigns</p>
                                 @if (session()->has('error'))
